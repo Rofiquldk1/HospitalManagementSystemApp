1c54e8525b259379f79070dbaf57dcc0f55f44c0
Rofiqul Islam <63155761+Rofiquldk1@users.noreply.github.com>
2022-06-04 18:08:36 +0600

    4/6/2022

---
+++ app/src/main/java/com/project/meetingapp/activities/PlotActivity.java
@@ -0,0 +1,186 @@
+package com.project.meetingapp.activities;
+
+
+import android.graphics.Color;
+import android.graphics.DashPathEffect;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
+import android.os.Bundle;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.LimitLine;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
+import com.github.mikephil.charting.utils.Utils;
+import com.project.meetingapp.MyMarkerView;
+import com.project.meetingapp.R;
+
+import java.util.ArrayList;
+
+public class PlotActivity extends AppCompatActivity {
+    private LineChart mChart;
+    private static String publicKey = "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCgFGVfrY4jQSoZQWWygZ83roKXWD4YeT2x2p41dGkPixe73rT2IW04glagN2vgoZoHuOPqa5and6kAmK2ujmCHu6D1auJhE2tXP+yLkpSiYMQucDKmCsWMnW9XlC5K7OSL77TXXcfvTvyZcjObEz6LIBRzs6+FqpFbUO9SJEfh6wIDAQAB";
+    private static String privateKey = "MIICdQIBADANBgkqhkiG9w0BAQEFAASCAl8wggJbAgEAAoGBAKAUZV+tjiNBKhlBZbKBnzeugpdYPhh5PbHanjV0aQ+LF7vetPYhbTiCVqA3a+Chmge44+prlqd3qQCYra6OYIe7oPVq4mETa1c/7IuSlKJgxC5wMqYKxYydb1eULkrs5IvvtNddx+9O/JlyM5sTPosgFHOzr4WqkVtQ71IkR+HrAgMBAAECgYAkQLo8kteP0GAyXAcmCAkA2Tql/8wASuTX9ITD4lsws/VqDKO64hMUKyBnJGX/91kkypCDNF5oCsdxZSJgV8owViYWZPnbvEcNqLtqgs7nj1UHuX9S5yYIPGN/mHL6OJJ7sosOd6rqdpg6JRRkAKUV+tmN/7Gh0+GFXM+ug6mgwQJBAO9/+CWpCAVoGxCA+YsTMb82fTOmGYMkZOAfQsvIV2v6DC8eJrSa+c0yCOTa3tirlCkhBfB08f8U2iEPS+Gu3bECQQCrG7O0gYmFL2RX1O+37ovyyHTbst4s4xbLW4jLzbSoimL235lCdIC+fllEEP96wPAiqo6dzmdH8KsGmVozsVRbAkB0ME8AZjp/9Pt8TDXD5LHzo8mlruUdnCBcIo5TMoRG2+3hRe1dHPonNCjgbdZCoyqjsWOiPfnQ2Brigvs7J4xhAkBGRiZUKC92x7QKbqXVgN9xYuq7oIanIM0nz/wq190uq0dh5Qtow7hshC/dSK3kmIEHe8z++tpoLWvQVgM538apAkBoSNfaTkDZhFavuiVl6L8cWCoDcJBItip8wKQhXwHp0O3HLg10OEd14M58ooNfpgt+8D8/8/2OOFaR0HzA+2Dm";
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_plot);
+        mChart = findViewById(R.id.chart);
+        mChart.setTouchEnabled(true);
+        mChart.setPinchZoom(true);
+        MyMarkerView mv = new MyMarkerView(getApplicationContext(), R.layout.custom_marker_view);
+        mv.setChartView(mChart);
+        mChart.setMarker(mv);
+        renderData();
+
+
+        /*String encryptedString = null;
+        try {
+            encryptedString = Base64.encodeToString(RSAUtil.encrypt("Dhiraj is the author", publicKey)
+                    ,Base64.DEFAULT);
+        } catch (BadPaddingException e) {
+            e.printStackTrace();
+        } catch (IllegalBlockSizeException e) {
+            e.printStackTrace();
+        } catch (InvalidKeyException e) {
+            e.printStackTrace();
+        } catch (NoSuchPaddingException e) {
+            e.printStackTrace();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        String decryptedString = RSAUtil.decrypt(encryptedString, privateKey);*/
+
+
+
+        /*RSAKeyPairGenerator keyPairGenerator = null;
+        try {
+            keyPairGenerator = new RSAKeyPairGenerator();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        assert keyPairGenerator != null;
+
+        byte[] byteArray = new byte[0];
+        byte[] byteArray2 = new byte[0];
+
+        try {
+            byteArray = keyPairGenerator.getPublicKey().getEncoded().toString().getBytes("UTF-16");
+            byteArray2 = keyPairGenerator.
+                    getPrivateKey().getEncoded().toString().getBytes("UTF-16");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+
+        String encodedString = Base64.encodeToString(byteArray,Base64.DEFAULT);
+        String encodedString2 = Base64.encodeToString(byteArray2,Base64.DEFAULT);*/
+
+        /*Log.d("pub",encodedString);
+        Log.d("pri",encodedString2);*/
+
+    }
+
+    public void renderData() {
+        LimitLine llXAxis = new LimitLine(10f, "Index 10");
+        llXAxis.setLineWidth(4f);
+        llXAxis.enableDashedLine(10f, 10f, 0f);
+        llXAxis.setLabelPosition(LimitLine.LimitLabelPosition.RIGHT_BOTTOM);
+        llXAxis.setTextSize(10f);
+
+        XAxis xAxis = mChart.getXAxis();
+        xAxis.enableGridDashedLine(10f, 10f, 0f);
+        xAxis.setAxisMaximum(15f); //10
+        xAxis.setAxisMinimum(0f);
+        xAxis.setDrawLimitLinesBehindData(true);
+
+        LimitLine ll1 = new LimitLine(215f, "Maximum Limit");
+        ll1.setLineWidth(4f);
+        ll1.enableDashedLine(10f, 10f, 0f);
+        ll1.setLabelPosition(LimitLine.LimitLabelPosition.RIGHT_TOP);
+        ll1.setTextSize(10f);
+
+        LimitLine ll2 = new LimitLine(70f, "Minimum Limit");
+        ll2.setLineWidth(4f);
+        ll2.enableDashedLine(10f, 10f, 0f);
+        ll2.setLabelPosition(LimitLine.LimitLabelPosition.RIGHT_BOTTOM);
+        ll2.setTextSize(10f);
+
+        YAxis leftAxis = mChart.getAxisLeft();
+        leftAxis.removeAllLimitLines();
+        leftAxis.addLimitLine(ll1);
+        leftAxis.addLimitLine(ll2);
+        leftAxis.setAxisMaximum(350f);
+        leftAxis.setAxisMinimum(0f);
+        leftAxis.enableGridDashedLine(10f, 10f, 0f);
+        leftAxis.setDrawZeroLine(false);
+        leftAxis.setDrawLimitLinesBehindData(false);
+
+        mChart.getAxisRight().setEnabled(false);
+        setData();
+    }
+
+    private void setData() {
+
+        ArrayList<Entry> values = new ArrayList<>();
+        values.add(new Entry(1, 50));
+        values.add(new Entry(2, 100));
+        values.add(new Entry(3, 80));
+        values.add(new Entry(4, 120));
+        values.add(new Entry(5, 110));
+        values.add(new Entry(7, 150));
+        values.add(new Entry(8, 250));
+        values.add(new Entry(9, 190));
+        values.add(new Entry(10, 50));
+        values.add(new Entry(11, 105));
+        values.add(new Entry(12, 100));
+        values.add(new Entry(13, 80));
+        values.add(new Entry(14, 120));
+        values.add(new Entry(15, 110));
+
+
+        LineDataSet set1;
+        if (mChart.getData() != null &&
+                mChart.getData().getDataSetCount() > 0) {
+            set1 = (LineDataSet) mChart.getData().getDataSetByIndex(0);
+            set1.setValues(values);
+            mChart.getData().notifyDataChanged();
+            mChart.notifyDataSetChanged();
+        } else {
+            set1 = new LineDataSet(values, "Sample Data");
+            set1.setDrawIcons(false);
+            set1.enableDashedLine(10f, 5f, 0f);
+            set1.enableDashedHighlightLine(10f, 5f, 0f);
+            set1.setColor(Color.DKGRAY);
+            set1.setCircleColor(Color.DKGRAY);
+            set1.setLineWidth(1f);
+            set1.setCircleRadius(3f);
+            set1.setDrawCircleHole(false);
+            set1.setValueTextSize(9f);
+            set1.setDrawFilled(true);
+            set1.setFormLineWidth(1f);
+            set1.setFormLineDashEffect(new DashPathEffect(new float[]{10f, 5f}, 0f));
+            set1.setFormSize(15.f);
+
+            if (Utils.getSDKInt() >= 18) {
+                Drawable drawable = ContextCompat.getDrawable(this, R.drawable.fade_blue);
+                set1.setFillDrawable(drawable);
+            } else {
+                set1.setFillColor(Color.DKGRAY);
+            }
+            ArrayList<ILineDataSet> dataSets = new ArrayList<>();
+            dataSets.add(set1);
+            LineData data = new LineData(dataSets);
+            mChart.setData(data);
+        }
+    }
+}
---
+++ app/src/main/java/com/project/meetingapp/activities/TActivity.java
@@ -0,0 +1,165 @@
+package com.project.meetingapp.activities;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
+
+import com.github.aakira.expandablelayout.ExpandableRelativeLayout;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthProvider;
+import com.hbb20.CountryCodePicker;
+import com.project.meetingapp.R;
+
+import java.util.concurrent.TimeUnit;
+
+public class TActivity extends AppCompatActivity {
+    private CountryCodePicker ccp;
+    private EditText Phonetext,Codetext;
+    private Button continueAndNextbtn;
+    String checker="",phoneNumber="";
+    private RelativeLayout relativeLayout;
+    private PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallbacks;
+    private FirebaseAuth mAuth;
+    private String mVerificationId;
+    private PhoneAuthProvider.ForceResendingToken mResndToken;
+    private ProgressDialog loadingbar;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tactivity);
+
+        mAuth = FirebaseAuth.getInstance();
+        loadingbar = new ProgressDialog(this);
+
+        ccp = findViewById(R.id.ccp);
+        Phonetext = findViewById(R.id.phoneText);
+        Codetext = findViewById(R.id.codeText);
+        continueAndNextbtn = findViewById(R.id.continueNextButton);
+        relativeLayout = findViewById(R.id.phoneAuth);
+
+        ccp.registerCarrierNumberEditText(Phonetext);
+
+        continueAndNextbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(continueAndNextbtn.getText().equals("Submit")||checker.equals("Code Sent")){
+                    String verificaticode = Codetext.getText().toString();
+                    if(verificaticode.equals("")){
+                        Toast.makeText(TActivity.this,"Please write verification code", Toast.LENGTH_LONG).show();
+                    }
+                    else{
+                        loadingbar.setTitle("Code Verification");
+                        loadingbar.setMessage("Please Wait,While we are Varyfing your code");
+                        loadingbar.setCanceledOnTouchOutside(false);
+                        loadingbar.show();
+
+                        PhoneAuthCredential credential = PhoneAuthProvider.getCredential(mVerificationId,verificaticode);
+                        signInWithPhoneAuthCredential(credential);
+                    }
+                }
+                else{
+                    //phoneNumber=ccp.getFullNumberWithPlus();
+                    phoneNumber= "+8801521506095";
+                    if(!phoneNumber.equals("")){
+                        loadingbar.setTitle("Phone Number Verification");
+                        loadingbar.setMessage("Please Wait,While we are Varyfing your phone number");
+                        loadingbar.setCanceledOnTouchOutside(false);
+                        loadingbar.show();
+
+                        PhoneAuthProvider.getInstance().verifyPhoneNumber(
+                                phoneNumber,        // Phone number to verify
+                                60,                 // Timeout duration
+                                TimeUnit.SECONDS,   // Unit of timeout
+                                TActivity.this,               // Activity (for callback binding)
+                                mCallbacks);        // OnVerificationStateChangedCallbacks
+                    }
+                    else{
+                        Toast.makeText(TActivity.this,"Please enter valid phone number",Toast.LENGTH_LONG).show();
+                    }
+                }
+            }
+        });
+
+        mCallbacks = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+            @Override
+            public void onVerificationCompleted(@NonNull PhoneAuthCredential phoneAuthCredential) {
+                signInWithPhoneAuthCredential(phoneAuthCredential);
+            }
+
+            @Override
+            public void onVerificationFailed(@NonNull FirebaseException e) {
+                Toast.makeText(TActivity.this,"Invalid phone number.....",Toast.LENGTH_LONG).show();
+                loadingbar.dismiss();
+                relativeLayout.setVisibility(View.VISIBLE);
+                continueAndNextbtn.setText("Continue");
+                Codetext.setVisibility(View.GONE);
+            }
+
+            @Override
+            public void onCodeSent(@NonNull String s, @NonNull PhoneAuthProvider.ForceResendingToken forceResendingToken) {
+                super.onCodeSent(s, forceResendingToken);
+                relativeLayout.setVisibility(View.GONE);
+                checker = "Code Sent";
+                continueAndNextbtn.setText("Submit");
+                Codetext.setVisibility(View.VISIBLE);
+
+                mVerificationId=s;
+                mResndToken=forceResendingToken;
+
+                loadingbar.dismiss();
+                Toast.makeText(TActivity.this,"Code has been sent,Please check...",Toast.LENGTH_LONG).show();
+
+            }
+        };
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+       /* FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        if(firebaseUser!=null){
+            Intent homeintent = new Intent(TActivity.this,MainActivity.class);
+            startActivity(homeintent);
+            finish();
+        }*/
+    }
+
+    private void signInWithPhoneAuthCredential(PhoneAuthCredential credential) {
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+
+                            loadingbar.dismiss();
+                            Toast.makeText(TActivity.this,"Congratulations,you are login successfully",Toast.LENGTH_LONG).show();
+                            sendUserToMainActivity();
+                        } else {
+                            loadingbar.dismiss();
+                            String e = task.getException().toString();
+                            Toast.makeText(TActivity.this,"Error: "+e,Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+    }
+    private void sendUserToMainActivity(){
+        Intent intent = new Intent(TActivity.this,MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
